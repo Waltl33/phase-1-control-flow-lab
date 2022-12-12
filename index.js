ce2ad9422e5785f3417a2c9c2fc74b31bad15cd3
Walter <walter.clark24@yahoo.com>
2022-12-12 15:15:52 -0500

    commit tree 55ac87bfbeda0bcb4e741f616465984a02a6d162

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  if(distance <= 400)
+  return "This one is on me!";
 
-function ternaryCheckCity(){
+} 
+// else if (distance > 400 && distance < 2000) {
+//   return "That will be twenty bucks."
+// }
+// } else if (distance > 2000) {
+//   return "That will be thirty bucks"
+
+function ternaryCheckCity(city){
   // Write your code here!
-}
+// const city = "NYC"
+let response 
 
+city==="NYC"? response = "Ok, sounds good." : response = "No go."
+return response;
+}
 function switchOnCharmFromTip(){
   // Write your code here!
+  let tip
+  if (tip === "generous") {
+    return "Thank you so much."
+  } else if (tip === "!generous"){
+    return "Thank you"
+  }else{
+    return "Bye."
+  }
 }
